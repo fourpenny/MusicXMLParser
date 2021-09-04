9630a6d3f5aea6f069e04b2a530c10ab092b74aa
Christian C <christianclark687@gmail.com>
2021-09-04 04:22:45 -0400

    Retrieve dictionary or JSON data from Score objects

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # MusicXMLParser
+
+A MusicXML Parser made in Python! Simply pass in each .musicxml file as an argument to the "parse_score()" function and call one of the retrieval methods, get_dict() or get_json(), on the Score object it creates. You can also use the "parse_many_scores()" function to get a dictionary of Score objects if you would like to retrieve data from multiple scores at once.
+
+## Install
+Download the "mxml_parser.py" file from this repo, then add it to your project directory. You should be able to use it as a module by including "import mxml_parser" at the top of the Python script you would like to use it in.
+
+## Features
+The Score object stores the following data:
+-The work's title
+-The work's composer
+-The work's instruments
+-The number of occurances of each pitch class
+
+Feel free to make a pull request if you would like to contribute any new features!
--- mxml_parser.py
+++ mxml_parser.py
@@ -1,5 +1,6 @@
 import xml.sax
 from collections import defaultdict
+import json
 
 class ScoreHander(xml.sax.ContentHandler):
     def __init__(self):
@@ -73,9 +74,46 @@ class Score():
         self.parts = kwargs['parts'] if 'parts' in kwargs else None
         self.pitch_list = kwargs['pitches'] if 'pitches' in kwargs else None
 
-handler = ScoreHander()
-parser = xml.sax.make_parser()
-parser.setContentHandler(handler)
-parser.parse('cinderella.musicxml')
+    def remove_pitch_list_tuples(self):
+        pitch_list_dict = {}
+        for pitch_tuple in self.pitch_list:
+            pitch = str(pitch_tuple[0]) + str(pitch_tuple[1])
+            count = self.pitch_list[pitch_tuple]
+            pitch_list_dict[pitch] = count
+        self.pitch_list = pitch_list_dict
 
-test_score = Score(parts=handler.parts, pitches=handler.note_count, composer=handler.composer, title=handler.title)
+    def get_json(self):
+        dictionary = self.get_dict()
+        json_data = json.dumps(dictionary, indent=4)
+        return json_data
+
+    def get_dict(self):
+        attr_dict = {}
+        attr_dict['title'] = self.title
+        attr_dict['composer'] = self.composer
+        attr_dict['parts'] = self.parts
+        if type(self.pitch_list) == defaultdict:
+            self.remove_pitch_list_tuples()
+        attr_dict['pitches'] = self.pitch_list
+        return attr_dict
+
+    def __str__(self):
+        return f'{self.title} was composed by {self.composer}'
+
+def parse_score(mxml_file):
+    handler = ScoreHander()
+    parser = xml.sax.make_parser()
+    parser.setContentHandler(handler)
+    parser.parse(mxml_file)
+    return Score(parts=handler.parts, pitches=handler.note_count, composer=handler.composer, title=handler.title)
+
+def parse_many_scores(file_list):
+    score_dict = {}
+    for file in file_list:
+        score = parse_score(file)
+        score_dict[score] = score
+    return score_dict
+
+#test_score = parse_score('test.musicxml')
+#test_json = test_score.get_json()
+#print(test_json)
